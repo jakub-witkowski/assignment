c6671a896d4779fdd6d4e65af1db3a15fc93a71e
kuba <j.witkowski@poczta.onet.pl>
2025-03-16 19:21:21 +0100

    modifications to further form classes

--- src/Form/UserWorkMonthSummaryFormType.php
+++ src/Form/UserWorkMonthSummaryFormType.php
@@ -2,7 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\User;
+use App\Entity\WorkTime;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,14 +15,20 @@ class UserWorkMonthSummaryFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('employee', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'getIdWithName',
+            ])
+            ->add('startMonth', TextType::class, [
+                'label' => 'Miesiąc pracy (YYYY-MM): '
+            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => WorkTime::class,
         ]);
     }
 }
