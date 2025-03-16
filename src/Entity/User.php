b86603fd99ea83f96781a5919293269a3a66a370
kuba <j.witkowski@poczta.onet.pl>
2025-03-16 19:00:17 +0100

    Entities and Repositories created

---
+++ src/Entity/User.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
+
+    /**
+     * @var Collection<int, WorkTime>
+     */
+    #[ORM\OneToMany(targetEntity: WorkTime::class, mappedBy: 'employee')]
+    private Collection $workTimes;
+
+    public function __construct()
+    {
+        $this->workTimes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, WorkTime>
+     */
+    public function getWorkTimes(): Collection
+    {
+        return $this->workTimes;
+    }
+
+    public function addWorkTime(WorkTime $workTime): static
+    {
+        if (!$this->workTimes->contains($workTime)) {
+            $this->workTimes->add($workTime);
+            $workTime->setEmployee($this);
+        }
+
+        return $this;
+    }
+
+    public function removeWorkTime(WorkTime $workTime): static
+    {
+        if ($this->workTimes->removeElement($workTime)) {
+            // set the owning side to null (unless already changed)
+            if ($workTime->getEmployee() === $this) {
+                $workTime->setEmployee(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/WorkTime.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WorkTimeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WorkTimeRepository::class)]
+class WorkTime
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'workTimes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $employee = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $startTime = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $endTime = null;
+
+    #[ORM\Column]
+    private ?int $workDuration = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $startDay = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $startMonth = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmployee(): ?User
+    {
+        return $this->employee;
+    }
+
+    public function setEmployee(?User $employee): static
+    {
+        $this->employee = $employee;
+
+        return $this;
+    }
+
+    public function getStartTime(): ?\DateTimeInterface
+    {
+        return $this->startTime;
+    }
+
+    public function setStartTime(\DateTimeInterface $startTime): static
+    {
+        $this->startTime = $startTime;
+
+        return $this;
+    }
+
+    public function getEndTime(): ?\DateTimeInterface
+    {
+        return $this->endTime;
+    }
+
+    public function setEndTime(\DateTimeInterface $endTime): static
+    {
+        $this->endTime = $endTime;
+
+        return $this;
+    }
+
+    public function getWorkDuration(): ?int
+    {
+        return $this->workDuration;
+    }
+
+    public function setWorkDuration(int $workDuration): static
+    {
+        $this->workDuration = $workDuration;
+
+        return $this;
+    }
+
+    public function getStartDay(): ?string
+    {
+        return $this->startDay;
+    }
+
+    public function setStartDay(string $startDay): static
+    {
+        $this->startDay = $startDay;
+
+        return $this;
+    }
+
+    public function getStartMonth(): ?string
+    {
+        return $this->startMonth;
+    }
+
+    public function setStartMonth(string $startMonth): static
+    {
+        $this->startMonth = $startMonth;
+
+        return $this;
+    }
+}
